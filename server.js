d6af8ea9df637bd723215a05b817475e462206e9
Narendra1234kathayat <nskthayat77@gmail.com>
2025-02-23 13:16:32 +0530

    FRONTEND SITE LINK

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "sqlmastervyom.netlify.app" }));
 app.use(bodyParser.json());
 
 // Database Connection
